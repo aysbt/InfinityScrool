42dd96412f622a1fee899a58ca50cc78255fc97b
Ayse Bat <aaysbt@gmail.com>
2022-08-04 06:59:04 +0300

    added the scroll time

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let imagesLoaded = 0 ;
+let totalImages = 0;
 let photosArray = [];
+let ready = false;
 
 //Unsplash API
 const count = 30;
 const apiKey = 'z76akkGjesKHhrvYl_Gjer0JDPNkDyLNtbTh_zpRwK0';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+//Check if all images were loaded
+function imageLoader() {
+console.log('image loaded')
+imagesLoaded++;
+if (imagesLoaded === totalImages) {
+    ready = true;
+    console.log('ready = ', ready);
+}
+}
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes (element, attributes) {
     for (const key in attributes) {
@@ -16,6 +28,8 @@ function setAttributes (element, attributes) {
 }
 //Create Element for Links & Photos, Add to DOM
 function displayPhotos() {
+    totalImages = photosArray.length;
+    console.log('total images= ', totalImages);
     //Run function for each object in photosArray
     photosArray.forEach((photo) => {
         //Create <a> to link to Unsplash
@@ -28,14 +42,12 @@ function displayPhotos() {
         });
         //Create <img> for photo
         const img = document.createElement('img');
-        // img.setAttribute('src', photo.urls.regular);
-        // img.setAttribute('alt', photo.alt_description);
-        // img.setAttribute('title', photo.alt_description);
         setAttributes(img, {
             src : photo.urls.regular,
             alt : photo.alt_description,
             title : photo.alt_description,
         });
+        img.addEventListener('load', imageLoader)
         //put <img> inside the <a>, then put both inside the imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
